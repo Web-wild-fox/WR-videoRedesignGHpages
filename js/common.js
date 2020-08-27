5903f83995f647c165acb6cbf6058a4031886814
Web-wild-fox <vrotmnenogi.12@mail.ru>
2020-08-27 13:22:50 +0400

    ВЁРСТКА+АДАПТИВ section-service Fix
    ВЁРСТКА+АДАПТИВ section-service Fix

--- js/common.js
+++ js/common.js
@@ -2,97 +2,64 @@ $(function () {
 
 	// Custom JS
 
-	// ===== Переключение тем для сайта__start =====//
-	// $(".light-theme").click(function () {
-	// 	$("body").attr('data-theme', 'light');
-	// });
-
-	// $(".dark-theme").click(function () {
-	// 	$("body").attr('data-theme', 'dark');
-	// });
-	// ===== Переключение тем для сайта__end =====//
-
-	// ===== Добавление оверлея для раскрытого мобильного меню__start =====//
-	// $('.navbar-toggler').click(function () {
-	// 	if ($(this).hasClass('collapsed')) {
-	// 		$('.overlay-nav').addClass('overlay-nav-open');
-	// 	} else {
-	// 		$('.overlay-nav').removeClass('overlay-nav-open');
-	// 	}
-	// });
-	// ===== Добавление оверлея для раскрытого мобильного меню__end =====//
-
-	// ===== Анимация "гамбургера" для мобильного меню__start =====//
-	// $('.navbar-toggler').click(function () {
-	// 	if ($(this).hasClass('collapsed')) {
-	// 		$('.hambergerIcon').addClass('open');
-	// 	} else {
-	// 		$('.hambergerIcon').removeClass('open');
-	// 	}
-	// });
-	// ===== Анимация "гамбургера" для мобильного меню__end =====//
-
 	// ===== Конвертация svg из тега в инлайновый код на странице для управления__start =====//
-	 jQuery('img.svg').each(function(){
+	jQuery('img.svg').each(function () {
 		var $img = jQuery(this);
 		var imgID = $img.attr('id');
 		var imgClass = $img.attr('class');
 		var imgURL = $img.attr('src');
 
-		jQuery.get(imgURL, function(data) {
-			 // Get the SVG tag, ignore the rest
-			 var $svg = jQuery(data).find('svg');
+		jQuery.get(imgURL, function (data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = jQuery(data).find('svg');
 
-			 // Add replaced image's ID to the new SVG
-			 if(typeof imgID !== 'undefined') {
-				  $svg = $svg.attr('id', imgID);
-			 }
-			 // Add replaced image's classes to the new SVG
-			 if(typeof imgClass !== 'undefined') {
-				  $svg = $svg.attr('class', imgClass+' replaced-svg');
-			 }
+			// Add replaced image's ID to the new SVG
+			if (typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass + ' replaced-svg');
+			}
 
-			 // Remove any invalid XML tags as per http://validator.w3.org
-			 $svg = $svg.removeAttr('xmlns:a');
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
 
-			 // Replace image with new SVG
-			 $img.replaceWith($svg);
+			// Replace image with new SVG
+			$img.replaceWith($svg);
 
 		}, 'xml');
 	});
 	// ===== Конвертация svg из тега в инлайновый код на странице для управления__end =====//
 
-	// ===== Анимация кнопки "up/down" для аккордиона в section-1__start =====//
-	// $('.sec-1__card_btn').click(function () {
-	// 	if ($(this).hasClass('collapsed')) {
-	// 		$('.sec-1__card_btn-icon', this).addClass('sec-1__card_btn-icon_open');
-	// 	} else {
-	// 		$('.sec-1__card_btn-icon', this).removeClass('sec-1__card_btn-icon_open');
-	// 	}
-	// });
-	// ===== Анимация кнопки "up/down" для аккордиона в section-1__end =====//
-
 	// ===== Плавная прокрутка в начало для кнопки наверх =====//
 
-	$(document).ready(function(){
+	$(document).ready(function () {
 		// = Вешаем событие прокрутки к нужному месту
-			//   на все ссылки якорь которых начинается на #
-			//  Для исключения ссылки из плавной прокрутки добавить .not('a[href^="#некая_ссылка"]') после $('a[href^="#"]')
-			$('a[href^="#"]').bind('click.smoothscroll',function (e) {
-				e.preventDefault();
-	
-				var target = this.hash,
+		//   на все ссылки якорь которых начинается на #
+		//  Для исключения ссылки из плавной прокрутки добавить .not('a[href^="#некая_ссылка"]') после $('a[href^="#"]')
+		$('a[href^="#"]').bind('click.smoothscroll', function (e) {
+			e.preventDefault();
+
+			var target = this.hash,
 				$target = $(target);
-	
-				$('html, body').stop().animate({
-					'scrollTop': $target.offset().top
-				}, 700, 'swing', function () {
-					window.location.hash = target;
-				});
+
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 700, 'swing', function () {
+				window.location.hash = target;
 			});
-	
 		});
 
+	});
+
 	// ===== Плавная прокрутка в начало для кнопки наверх_end =====//
+
+
+	// ===== Сброс проигрывания видео в модальном окне при его закрытии (по id) =====//
+	$('#service_modal-video-1, #service_modal-video-2, #service_modal-video-3').click(function () {
+		$('iframe').attr('src', $('iframe').attr('src'));
+	});
+	// ===== Сброс проигрывания видео в модальном окне при его закрытии (по id)_end =====//
 
 });
